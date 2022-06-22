47da975ced9666b9d1b0d2bf4c9720ee6aca21e6
leslieraquel <raquellezs@hotmail.com>
2022-06-21 22:00:06 -0500

    variables js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+//let se usa para cuando una variable va a ser cambiada ejemplo
+let nombre = 'Leslie';
+let apellido = 'Guaman';
+nombre = 'raquel'; 
+console.log(nombre);// se imprimira el valor de raquel ya que le estoy asignando un nuevo valor despues de declararlo
+// se usa const cuando la variable no puede ser cambiada 
+if(true){
+    const nombre = 'leslie';
+    console.log(nombre)
+}
